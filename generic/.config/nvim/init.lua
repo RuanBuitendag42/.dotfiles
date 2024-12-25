c109c6ea2403e67b2d3cfc778d59e49087517c58
Ruan Buitendag <ruan@minm.co.za>
2024-12-25 08:44:10 +0200

    nvim, nushell

--- generic/.config/nvim/init.lua
+++ generic/.config/nvim/init.lua
@@ -1,8 +1,2 @@
-vim.cmd([[
-  highlight NotifyBackground guibg=#000000
-]])
-
-require("ruanb.core.keymaps")
-require("ruanb.core.options")
-require("ruanb.core.autocmds")
-require("ruanb.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
